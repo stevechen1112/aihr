f82420100e449d19720e24d2d6b66f2cd42459b7
Steve Chen <stevechen1112@gmail.com>
2026-02-23 21:51:41 +0800

    fix: allow extra env vars in Settings (extra=ignore)

--- app/config.py
+++ app/config.py
@@ -107,7 +107,8 @@ class Settings(BaseSettings):
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
-        case_sensitive=True
+        case_sensitive=True,
+        extra="ignore",
     )
 
     @model_validator(mode="after")
